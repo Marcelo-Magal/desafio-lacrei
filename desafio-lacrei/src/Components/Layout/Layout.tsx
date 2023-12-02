3f1f432da1cec5829ae69c93b483b0d123d58147
Marcelo Carvalho <marcelomagal@gmail.com>
2023-12-02 13:34:33 -0300

    Remove PageContainer, otimiza estrutura do layout e prepara para ajustes de responsividade

--- desafio-lacrei/src/Components/Layout/Layout.tsx
+++ desafio-lacrei/src/Components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import PageContainer from "../PageContainer/PageContainer";
 import { StyledLayout } from "./styles";
 
 interface LayoutProps {
@@ -12,7 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <StyledLayout>
       <Header />
-      <PageContainer>{children}</PageContainer>
+      {children}
       <Footer />
     </StyledLayout>
   );
